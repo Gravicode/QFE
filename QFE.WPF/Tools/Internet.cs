2d640c1c3fef6442bd6bf8fc54a98e4408807a56
mifmasterz <mifmasterz@yahoo.com>
2016-06-20 16:36:26 +0700

    compiled
    dont forget to extract DB in debug folder

---
+++ QFE.WPF/Tools/Internet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QFE.WPF.Tools
+{
+    class Internet
+    {
+        public static async Task<bool> CheckConnection(String URL)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+                request.Timeout = 5000;
+                request.Credentials = CredentialCache.DefaultNetworkCredentials;
+                HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync();
+
+                if (response.StatusCode == HttpStatusCode.OK) return true;
+                else return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
